6fa2199a02a869f9acc3e44a75fbf311adb062d1
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2009-08-28 01:26:42 +0000

    Add websafe title

--- philblog/models.py
+++ philblog/models.py
@@ -5,6 +5,7 @@ import datetime
 # Create your models here.
 class Post(models.Model):
 	title = models.CharField(max_length=200)
+	websafe_title = models.CharField(max_length=200, editable=False)
 	body = models.TextField()
 	pub_date = models.DateTimeField('date published')
 	author = models.ForeignKey(User)
@@ -12,3 +13,7 @@ class Post(models.Model):
 		return self.title
 	def was_published_today(self):
 		return self.pub_date.date() == datetime.date.today()
+	def save(self, force_insert=False, force_update=False):
+		unclean_title = self.title
+		self.websafe_title = unclean_title.replace(' ', '_').strip("'\",.").lower()
+		super(Post, self).save(force_insert, force_update)
