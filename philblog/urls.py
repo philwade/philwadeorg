b41c243878b84d40bfc9185ddc4ae80484a7cd43
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2009-09-19 04:19:34 +0000

    Add pagination

--- philblog/urls.py
+++ philblog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('philwadeorg.philblog.views',
 	(r'^$', 'index'),
+	(r'^page/(?P<page>\d+)/$', 'index'),
 	(r'^post/(?P<websafe_title>.*)/$', 'detail'),
 )
--- philblog/views.py
+++ philblog/views.py
@@ -2,12 +2,19 @@
 from philwadeorg.philblog.models import Post
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
-def index(request):
-	latest_posts = Post.objects.all().order_by('-pub_date')[:5]
-	return render_to_response('philblog/index.html', {'latest_posts' : latest_posts})
+def index(request, page=1):
+	latest_posts = Post.objects.all().order_by('-pub_date')
+	paginator = Paginator(latest_posts, 2)
+	try:
+		posts = paginator.page(page)
+	except (EmptyPage, InvalidPage):
+		posts = paginator.page(paginator.num_pages)
+
+	return render_to_response('philblog/index.html', {'posts' : posts})
 
 def detail(request, websafe_title):
 	p = get_object_or_404(Post, websafe_title=websafe_title)
