ad342fcb1e521046401a1eeb5f5b00a1a82da591
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2009-09-27 20:10:15 +0000

    Fix the  pygment code highlighter, use my own stuff

--- helperapp/templatetags/pygment_tags.py
+++ helperapp/templatetags/pygment_tags.py
@@ -1,31 +1,25 @@
 from django import template
+from BeautifulSoup import BeautifulSoup
 import re
 import pygments
 import pygments.lexers as lexers
 import pygments.formatters as formatters
+from pygments.formatters import HtmlFormatter
 
 register = template.Library()
-
-regex = re.compile(r'<code>(.*?)</code>', re.DOTALL)
 
 @register.filter(name='pygmentize')
 def pygmentize(value):
     try:
-        last_end = 0
-        to_return = ''
-        found = 0
-        for match_obj in regex.finditer(value):
-            code_string = match_obj.group(1)
-            try:
-                lexer = lexers.guess_lexer(code_string)
-            except ValueError:
-                lexer = lexers.PythonLexer()
-            pygmented_string = pygments.highlight(code_string, lexer, formatters.HtmlFormatter())
-            to_return = to_return + value[last_end:match_obj.start(1)] + pygmented_string
-            last_end = match_obj.end(1)
-            found = found + 1
-        to_return = to_return + value[last_end:]
-        to_return += u"<style>%s</style>" % formatters.HtmlFormatter().get_style_defs('.highlight')
-        return to_return
-    except:
-        return value
+        formatter = HtmlFormatter(style='trac')
+        tree = BeautifulSoup(value)
+        for code in tree.findAll('code'): 
+            if not code['class']: code['class'] = 'text'
+            lexer = lexers.get_lexer_by_name(code['class'])
+            new_content = pygments.highlight(code.contents[0], lexer, formatter)
+            new_content += u"<style>%s</style>" % formatter.get_style_defs('.highlight')
+            code.replaceWith ( "%s\n" % new_content )
+        content = str(tree)
+        return content
+    except KeyError:
+      return value
