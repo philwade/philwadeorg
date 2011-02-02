e01b206e45c6a6561db59b06ad02fa47c7490559
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2011-02-02 18:45:47 +0000

    Fix syntax highlighting hopefully for the last time

--- helperapp/templatetags/pygment_tags.py
+++ helperapp/templatetags/pygment_tags.py
@@ -9,15 +9,15 @@ register = template.Library()
 @register.filter(name='pygmentize')
 def pygmentize(value):
     try:
-        formatter = formatters.HtmlFormatter(style='trac')
-        tree = BeautifulSoup(value)
-        for code in tree.findAll('code'): 
-            if not code['class']: code['class'] = 'text'
-            lexer = lexers.get_lexer_by_name(code['class'])
-            new_content = pygments.highlight(code.decodeContents(), lexer, formatter)
-            new_content += u"<style>%s</style>" % formatter.get_style_defs('.highlight')
-            code.replaceWith ( "%s\n" % new_content )
-        content = str(tree)
-        return content
+        my_formatter = formatters.HtmlFormatter(style='trac')
+        my_tree = BeautifulSoup(value)
+        for code_block in my_tree.findAll('code'): 
+            if not code_block['class']: code_block['class'] = 'text'
+            my_lexer = lexers.get_lexer_by_name(code_block['class'])
+            new_content = pygments.highlight(code_block.text, my_lexer, my_formatter)
+            new_content += u"<style>%s</style>" % my_formatter.get_style_defs('.highlight')
+            code_block.replaceWith ( "%s\n" % new_content )
+        my_content = str(my_tree)
+        return my_content
     except KeyError:
       return value
