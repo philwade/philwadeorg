3ae1dc92d47481edb406538a2ef8f5c3144f31f8
phil <phil@6596c492-bec3-4f50-a8a2-b0ff2d2738bf>
2012-02-24 17:02:07 +0000

    Make comments work again

--- philblog/views.py
+++ philblog/views.py
@@ -5,17 +5,20 @@ from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.template import RequestContext
 
 def index(request, page=1):
-	latest_posts = Post.objects.all().order_by('-pub_date')
-	paginator = Paginator(latest_posts, 5)
-	try:
-		posts = paginator.page(page)
-	except (EmptyPage, InvalidPage):
-		posts = paginator.page(paginator.num_pages)
+    latest_posts = Post.objects.all().order_by('-pub_date')
+    paginator = Paginator(latest_posts, 5)
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        posts = paginator.page(paginator.num_pages)
 
-	return render_to_response('philblog/index.html', {'posts' : posts})
+    return render_to_response('philblog/index.html', {'posts' : posts})
 
 def detail(request, websafe_title):
-	p = get_object_or_404(Post, websafe_title=websafe_title)
-	return render_to_response('philblog/detail.html', {'post' : p})
+    p = get_object_or_404(Post, websafe_title=websafe_title)
+    post_data = { 'post' : p }
+    csrfContext = RequestContext(request, post_data)
+    return render_to_response('philblog/detail.html', csrfContext)
